660f4a7634754742881f84565cc6a0570d4ae11a
Tomi <Tomi@LAPTOP-2HJRIFH8.wlan.metropolia.fi>
2017-02-26 14:46:23 +0200

    inventory

---
+++ 3DGame_engine/src/game/engine/interfaces/InventoryInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.engine.interfaces;
+
+/**
+ *
+ * @author Tomi
+ */
+public class InventoryInterface {
+    
+    public InventoryInterface(){
+        
+    }
+}
